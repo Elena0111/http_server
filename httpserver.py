aa9db0bb7ec1da7872c6721a0d3e2996c10fa808
Elena0111 <elena.06filippini@gmail.com>
2026-07-04 19:02:10 +0200

    refactor

--- httpserver.py
+++ httpserver.py
@@ -11,10 +11,10 @@ class WSGIServer(object):
     def __init__(self, server_address):
         # Define socket host and port
         SERVER_HOST = '0.0.0.0'
-        SERVER_PORT = 8000
+        SERVER_PORT = 8888
 
         # Create socket:
-        listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.listen_socket = listen_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         listen_socket.bind((SERVER_HOST, SERVER_PORT))
         listen_socket.listen(1)
@@ -51,10 +51,24 @@ class WSGIServer(object):
         
             except FileNotFoundError:
                 response = 'HTTP/1.0 404 NOT FOUND\n\nFile Not Found'
-        #   Send HTTP response
+            #   Send HTTP response
     
-        client_connection.sendall(response.encode())
-        # Close socket
-        client_connection.close()
+            client_connection.sendall(response.encode())
+            # Close socket
+            client_connection.close()
 
+def make_server(server_address, application):
+    server = WSGIServer(server_address)
+    server.set_app(application)
+    return server
 
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.exit('Provide a WSGI application object as module:callable')
+    app_path = sys.argv[1]
+    module, application = app_path.split(':')
+    module = __import__(module)
+    application = getattr(module, application)
+    httpd = make_server(('', 8888), application)
+    print('WSGIServer: Serving HTTP on port  ...\n')
+    httpd.serve_forever()
