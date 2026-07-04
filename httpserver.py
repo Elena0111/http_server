4d7ffc215878d1e9830fe93422ba95f8faed5133
Elena0111 <elena.06filippini@gmail.com>
2026-07-04 17:44:21 +0200

    rename variables

--- httpserver.py
+++ httpserver.py
@@ -11,22 +11,22 @@ SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 8000
 
 # Create socket:
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((SERVER_HOST, SERVER_PORT))
-server_socket.listen(1)
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket.bind((SERVER_HOST, SERVER_PORT))
+listen_socket.listen(1)
 print('Listening on port %s ...' % SERVER_PORT)
 
 while True:
     # Wait for client connections
-    client_connection, client_address = server_socket.accept()
+    client_connection, client_address = listen_socket.accept()
 
     # Get the client request
-    request = client_connection.recv(1024).decode()
-    print(request)
+    request_data = client_connection.recv(1024).decode()
+    print(request_data)
 
 # Parse HTTP headers
-    headers = request.split('\n')
+    headers = request_data.split('\n')
     filename = headers[0].split()[1]
 
     # Get the content of the file
@@ -43,10 +43,7 @@ while True:
     # Send HTTP response
     
     client_connection.sendall(response.encode())
-    # Get the content of htdocs/index.html
-  
+    # Close socket
+    client_connection.close()
 
-   
 
-# Close socket
-server_socket.close()
